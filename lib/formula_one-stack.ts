3af944481384178fd1e7aa4f0e7508499f001c99
Caleb Ekstrand <caleb.ekstrand@gmail.com>
2022-06-25 21:53:08 -0500

    Reset lambda code.
    Removeing lambda definitions to reset.

--- lib/formula_one-stack.ts
+++ lib/formula_one-stack.ts
@@ -1,9 +1,6 @@
 import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-
-const lambdaDir = lambda.Code.fromAsset('./lambda');
 
 export class FormulaOneStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,38 +12,5 @@ export class FormulaOneStack extends Stack {
       tableName : 'raceData',
       removalPolicy : RemovalPolicy.DESTROY
     });
-
-    const buildItemFunction = new lambda.Function(this, 'buildItem', {
-      functionName: 'buildItem',
-      code : lambdaDir,
-      handler : 'webscrape.buildItem',
-      runtime : lambda.Runtime.NODEJS_16_X
-    });
-    buildItemFunction.applyRemovalPolicy(RemovalPolicy.DESTROY);
-    
-    const getYearDataFunction = new lambda.Function(this, 'getYearData', {
-      functionName: 'getYearData',
-      code : lambdaDir,
-      handler : 'webscrape.getYearData',
-      runtime : lambda.Runtime.NODEJS_16_X,
-      environment : {
-        DOWN_STREAM_FUNCTION_NAME : buildItemFunction.functionName
-      }
-    });
-    getYearDataFunction.applyRemovalPolicy(RemovalPolicy.DESTROY);
-    
-    const scrapeDataFunction = new lambda.Function(this, 'scrapeData', {
-      functionName : 'scrapeData',
-      code : lambdaDir,
-      handler : 'webscrape.scrapeData',
-      runtime : lambda.Runtime.NODEJS_16_X,
-      environment : {
-        DOWN_STREAM_FUNCTION_NAME : getYearDataFunction.functionName,
-        RACE_TABLE_NAME : raceTable.tableName
-      }
-    });
-    scrapeDataFunction.applyRemovalPolicy(RemovalPolicy.DESTROY);
-    
-    raceTable.grantReadWriteData(buildItemFunction);
   }
 }
