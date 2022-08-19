ad0592907367e28026d32729d66779d12a9fdf49
Caleb Ekstrand <caleb.ekstrand@gmail.com>
2022-08-18 23:47:39 -0500

    Fixed typescript build

--- lib/formula-one-data-load-stack.ts
+++ lib/formula-one-data-load-stack.ts
@@ -1,16 +1,28 @@
 import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class FormulaOneDataLoadStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
+    const lambdaDir = path.join(__dirname, './../lambda/');
+
     const raceTable = new dynamo.Table(this, 'raceData', {
       partitionKey : { name : 'year_grandPrix', type : dynamo.AttributeType.STRING },
       sortKey : { name : 'session_driver', type : dynamo.AttributeType.STRING },
       tableName : 'race-data-table',
       removalPolicy : RemovalPolicy.DESTROY
+    });
+
+    const grandPrixProcessor = new NodejsFunction(this, 'grand-prix-processor', {
+      functionName: 'grand-prix-processor',
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: './lambda/grand-prix-processor.ts',
+      handler: 'handler'
     });
   }
 }
